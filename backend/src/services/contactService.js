84eb690fa20035ad867bf824056a35ac77b708b4
VeironVaya <veironvayayarief41@gmail.com>
2025-07-04 14:21:46 +0700

    minus deploy v2

--- backend/src/services/contactService.js
+++ backend/src/services/contactService.js
@@ -2,17 +2,18 @@ const nodemailer = require('nodemailer');
 
 exports.sendMail = async ({ name, email, subject, message }) => {
 
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',       
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
     }
   });
 
   const mailOptions = {
     from: `"${name}" <${email}>`,
-    to:   process.env.EMAIL_USER,
+    to:   process.env.SMTP_USER,
     subject: subject || 'New message from contact form',
     text: `
 Name: ${name}
